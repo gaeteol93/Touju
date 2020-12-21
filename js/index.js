3116b8127e7815cae059558ff45ebcd8824b5533
gaeteol <gaeteol93@naver.com>
2020-12-21 18:34:03 +0900

    upload files

--- js/index.js
+++ js/index.js
@@ -5,9 +5,7 @@ $(".more_Cts2").hide();
 $(".more_Cts3").hide();
 
 // 숨김버튼
-$("#less_Btn1").hide();
-$("#less_Btn2").hide();
-$("#less_Btn3").hide();
+$("#less_Btn").hide();
 
 // 더보기 버튼
 $("#more_Btn2").hide();
@@ -18,8 +16,6 @@ $("#more_Btn1").click(function() {
     $("#more_Btn1").hide();
     $("#more_Btn2").show();
 
-    $("#less_Btn1").show();
-
     $(".more_Cts").show();
 })
 // 더보기 버튼 2
@@ -27,48 +23,20 @@ $("#more_Btn2").click(function() {
     $(".more_Cts2").show();
 
     $("#more_Btn2").hide();
-    $("#more_Btn3").show();
 
-    $("#less_Btn1").hide();
-    $("#less_Btn2").show();
-})
-// 더보기 버튼 3
-$("#more_Btn3").click(function() {
-    $(".more_Cts3").show();
-
-    $("#more_Btn3").hide();
-
-    $("#less_Btn2").hide();
-    $("#less_Btn3").show();
+    $("#less_Btn").show();
 })
 
-// 숨기기 버튼 1
-$("#less_Btn1").click(function() {
+
+// 숨기기 버튼
+$("#less_Btn").click(function() {
+    $(".more_Cts3").hide();
+    $(".more_Cts2").hide();
     $(".more_Cts").hide();
 
     $("#more_Btn1").show();
-    $("#more_Btn2").hide();
 
-    $("#less_Btn1").hide();
-})
-// 숨기기 버튼 2
-$("#less_Btn2").click(function() {
-    $(".more_Cts2").hide();
-
-    $("#more_Btn2").show();
-    $("#more_Btn3").hide();
-
-    $("#less_Btn1").show();
-    $("#less_Btn2").hide();
-})
-// 숨기기 버튼 3
-$("#less_Btn3").click(function() {
-    $(".more_Cts3").hide();
-
-    $("#more_Btn3").show();
-
-    $("#less_Btn2").show();
-    $("#less_Btn3").hide();
+    $("#less_Btn").hide();
 })
 
 
@@ -105,3 +73,6 @@ $(".heart_on").click(function() {
 
     $(".heart_off").show();
 })
+
+
+$(".video_cts02").hide();
